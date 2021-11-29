659105197bd90c03e407c7ffe650b72330dd14c0
Danny Götte <danny.goette@fem.tu-ilmenau.de>
2021-11-29 22:33:56 +0100

    Add Dart Hello World

---
+++ README.md
@@ -0,0 +1,4 @@
+AdventOfCode 2021
+=================
+
+After two years of Kotlin, now trying Dart for [advent of code 2021](https://adventofcode.com/2021)
---
+++ test/adventofcode2021_test.dart
@@ -0,0 +1,8 @@
+import 'package:adventofcode2021/adventofcode2021.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
