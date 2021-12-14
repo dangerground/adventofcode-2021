61bad4a1a32e2c6bb2f6ba7988cac4f874dcfafe
Danny Götte <danny.goette@fem.tu-ilmenau.de>
2021-12-14 07:55:18 +0100

    Refactoring Day14: Replace part 1 impl by part2 improvements

--- lib/day14.dart
+++ lib/day14.dart
@@ -11,49 +11,17 @@ void main() {
 }
 
 int part1(List<List<String>> batches) {
-  var template = batches[0][0];
-  var pairMappings = <String, String>{};
-  for (var value in batches[1].map((e) => e.split(" -> "))) {
-    pairMappings[value[0]] = value[1];
-  }
+  var pairs = getInitialPairs(batches);
+  var pairMappings = getPairInserts(batches);
+
   var steps = 10;
   for (var i = 0; i < steps; i++) {
-    var inserts = <int, String>{};
-    pairMappings.forEach((pattern, insert) {
-      var lastFound = 0;
-      do {
-        var x = template.indexOf(pattern, lastFound);
-        if (x > -1) {
-          inserts[x + 1] = insert;
-        }
-        lastFound = x + 1;
-      } while (lastFound > 0);
-    });
-
-    var sortedInserts = inserts.keys.toList();
-    sortedInserts.sort((a, b) => a.compareTo(b));
-
-    var chars = template.chars().toList();
-    sortedInserts.reversed.forEach((index) {
-      chars.insert(index, inserts[index]!);
-    });
-
-    template = chars.join();
+    pairs = insertMappings(pairs, pairMappings);
   }
 
-  var counts = <String, int>{};
-  template.chars().forEach((element) {
-    counts[element] = (counts[element] ?? 0) + 1;
-  });
+  var corrected = countChars(pairs);
 
-  var most = 0;
-  var uncommon = counts.values.first;
-  counts.forEach((key, value) {
-    most = max(most, value);
-    uncommon = min(uncommon, value);
-  });
-
-  return most - uncommon;
+  return diffBetweenMostAndRarestChar(corrected);
 }
 
 int part2(List<List<String>> batches) {
