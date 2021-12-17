c02cd0eccf30c50a455739afde9b2b0423d34eed
Danny Götte <danny.goette@fem.tu-ilmenau.de>
2021-12-17 07:05:12 +0100

    Refactoring Day 17: Simplify Logic

--- lib/day17.dart
+++ lib/day17.dart
@@ -17,30 +17,19 @@ int solve(int xMin, int xMax, int yMin, int yMax, {int part = 1}) {
     for (int y = yMin * -1; y >= yMin; y--) {
       var pos = Point<int>(0, 0);
       var diff = Point<int>(x, y);
-      var inTarget = false;
       var localHighest = 0;
       do {
         localHighest = max(localHighest, pos.y);
 
         if (inTargetArea(pos, xMin, xMax, yMin, yMax)) {
-          inTarget = true;
+          options.add(Point(x, y));
+          highest = max(highest, localHighest);
+          break;
         }
 
         pos = Point(pos.x + diff.x, pos.y + diff.y);
-        if (diff.x != 0) {
-          diff = Point(diff.x - (diff.x ~/ diff.x.abs()), diff.y - 1);
-        } else {
-          if (pos.y < yMin) {
-            break;
-          }
-          diff = Point(0, diff.y - 1);
-        }
-      } while (pos.x <= xMax);
-
-      if (inTarget) {
-        options.add(Point(x, y));
-        highest = max(highest, localHighest);
-      }
+        diff = Point(max(diff.x - 1, 0), diff.y - 1);
+      } while (pos.x <= xMax && pos.y >= yMin);
     }
   }
 
