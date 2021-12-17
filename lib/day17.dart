59b44560f00bf329b3ed89fedb3d9278a16fccc6
Danny Götte <danny.goette@fem.tu-ilmenau.de>
2021-12-17 06:59:35 +0100

    Refactoring Day 17: Use unified Codebase for both parts

--- lib/day17.dart
+++ lib/day17.dart
@@ -4,33 +4,28 @@ import 'package:adventofcode2021/util/input.dart';
 import 'package:quantity/quantity.dart';
 
 void main() {
-  final input = readLinesAsString(17);
   // target area: x=96..125, y=-144..-98
 
-  print("Solution part1: [ ${part1(96, 125, -144, -98)} ]");
-  print("Solution part2: [ ${part2(96, 125, -144, -98)} ]");
+  print("Solution part1: [ ${solve(96, 125, -144, -98, part: 1)} ]");
+  print("Solution part2: [ ${solve(96, 125, -144, -98, part: 2)} ]");
 }
 
-int part1(int xMin, int xMax, int yMin, int yMax) {
-
+int solve(int xMin, int xMax, int yMin, int yMax, {int part = 1}) {
   var options = <Point>{};
-var  heighest = 0;
+  var highest = 0;
   for (int x = xMax; x > 0; x--) {
-    for (int y = yMin*-1; y >= 0; y--) {
-  //var  x = 9;
-  //var  y = 0;
+    for (int y = yMin * -1; y >= yMin; y--) {
       var pos = Point<int>(0, 0);
       var diff = Point<int>(x, y);
-      Point<int>? lastIn;
-      var possible = 0;
+      var inTarget = false;
+      var localHighest = 0;
       do {
-//        print("pos $pos - $diff");
-        if (pos.x >= xMin && pos.x <= xMax && pos.y >= yMin && pos.y <= yMax) {
-          //print("found $pos");
-          lastIn = pos;
+        localHighest = max(localHighest, pos.y);
+
+        if (inTargetArea(pos, xMin, xMax, yMin, yMax)) {
+          inTarget = true;
         }
 
-        possible = max(possible, pos.y);
         pos = Point(pos.x + diff.x, pos.y + diff.y);
         if (diff.x != 0) {
           diff = Point(diff.x - (diff.x ~/ diff.x.abs()), diff.y - 1);
@@ -40,58 +35,21 @@ var  heighest = 0;
           }
           diff = Point(0, diff.y - 1);
         }
-
       } while (pos.x <= xMax);
-      if (lastIn != null) {
+
+      if (inTarget) {
         options.add(Point(x, y));
-        heighest = max(heighest, possible);
-      }
-    }
-  }
-  print(heighest);
-
-  return heighest;
-}
-
-int part2(int xMin, int xMax, int yMin, int yMax) {
-
-  var options = <Point>{};
-  var  heighest = 0;
-  for (int x = xMax; x > 0; x--) {
-    for (int y = yMin*-1; y >= yMin; y--) {
-      //var  x = 9;
-      //var  y = 0;
-      var pos = Point<int>(0, 0);
-      var diff = Point<int>(x, y);
-      Point<int>? lastIn;
-      var possible = 0;
-      do {
-//        print("pos $pos - $diff");
-        if (pos.x >= xMin && pos.x <= xMax && pos.y >= yMin && pos.y <= yMax) {
-          //print("found $pos");
-          lastIn = pos;
-        }
-
-        possible = max(possible, pos.y);
-        pos = Point(pos.x + diff.x, pos.y + diff.y);
-        if (diff.x != 0) {
-          diff = Point(diff.x - (diff.x ~/ diff.x.abs()), diff.y - 1);
-        } else {
-          if (pos.y < yMin) {
-            break;
-          }
-          diff = Point(0, diff.y - 1);
-        }
-
-      } while (pos.x <= xMax);
-      if (lastIn != null) {
-        options.add(Point(x, y));
-        heighest = max(heighest, possible);
+        highest = max(highest, localHighest);
       }
     }
   }
 
-//  print(options);
-
-  return options.length;
+  if (part == 1) {
+    return highest;
+  } else {
+    return options.length;
+  }
 }
+
+bool inTargetArea(Point<int> pos, int xMin, int xMax, int yMin, int yMax) =>
+    pos.x >= xMin && pos.x <= xMax && pos.y >= yMin && pos.y <= yMax;
--- test/day17_test.dart
+++ test/day17_test.dart
@@ -6,7 +6,7 @@ void main() {
   group("part1", () {
     test('p1 - given example', () {
       // when
-      final result = part1(20, 30, -10, -5);
+      final result = solve(20, 30, -10, -5, part: 1);
 
       // then
       expect(result, equals(45));
@@ -16,7 +16,7 @@ void main() {
   group("part2", () {
     test('p2 - given example', () {
       // when
-      final result = part2(20, 30, -10, -5);
+      final result = solve(20, 30, -10, -5, part: 2);
 
       // then
       expect(result, equals(112));
